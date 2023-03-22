6f28fa30bfa6a2f9d17d74cf069da3189ab34355
Matias Cejas <matiascejas8@gmail.com>
2023-03-22 13:29:58 -0300

    feat: remove console.log

--- src/utils/arithmetic/addition.ts
+++ src/utils/arithmetic/addition.ts
@@ -1,7 +1,5 @@
 import boom from '@hapi/boom';
 export default function addition(a: number, b: number | null) {
-  console.log({ a, b });
-
   if ((!b && b !== 0) || (!a && a !== 0)) {
     throw boom.badRequest(
       'You must fill in all the fields to perform this operation.',
